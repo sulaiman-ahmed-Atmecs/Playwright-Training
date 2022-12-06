51f9fb0721fc6286cb23ae300f6b2dc0be441d28
sulaimanahmedz-katabat <98012991+sulaimanahmedz-katabat@users.noreply.github.com>
2022-12-06 18:15:12 +0530

    Added day 2 folder and code

---
+++ Dec 6th - Day 2/app.ts
@@ -0,0 +1,13 @@
+console.log("Hello world!!!");
+// Number type in typescript
+var num1 : number;
+num1= 12;
+console.log("Number Type : "+num1);
+
+// String type in typescript
+let str : string;
+str= "Hello World!";
+console.log("String Type : "+str);
+
+const truthyOrFalsyValue: boolean = false;
+console.log("Boolean Type: " + truthyOrFalsyValue);
