fd9e5ff721185d02823cf896c82dee299e16c01a
sulaimanahmedz-katabat <98012991+sulaimanahmedz-katabat@users.noreply.github.com>
2022-12-06 18:08:58 +0530

    Added day 1 files

---
+++ Dec 5th - Day 1/app.ts
@@ -0,0 +1,12 @@
+"use strict";
+console.log("Hello world!!!");
+// Number type in typescript
+var num1;
+num1 = 12;
+console.log("Number Type : " + num1);
+// String type in typescript
+let str;
+str = "Hello World!";
+console.log("String Type : " + str);
+const truthyOrFalsyValue = false;
+console.log("Boolean Type: " + truthyOrFalsyValue);
