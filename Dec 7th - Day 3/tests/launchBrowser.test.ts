c1a4cfb385748d95c0b21c0881ae1bef70171487
sulaimanahmedz-katabat <98012991+sulaimanahmedz-katabat@users.noreply.github.com>
2022-12-07 18:07:23 +0530

    Changed the variable name.

--- Dec 7th - Day 3/tests/launchBrowser.test.ts
+++ Dec 7th - Day 3/tests/launchBrowser.test.ts
@@ -2,12 +2,12 @@ import { chromium } from "playwright";
 
 describe("Sample run", () => {
     test("Open playwright", async () => {
-       const browserrrr =   await chromium.launch({
+       const browser =   await chromium.launch({
         headless: false, 
        });
-       const context = await browserrrr.newContext();
+       const context = await browser.newContext();
        const page = await context.newPage();
        await page.goto("https://www.google.com");
-       await browserrrr.close();
+       await browser.close();
     });
 });
