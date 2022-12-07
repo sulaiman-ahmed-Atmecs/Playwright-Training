e650dc070d5100bedeb83086676872ca03178d68
sulaimanahmedz-katabat <98012991+sulaimanahmedz-katabat@users.noreply.github.com>
2022-12-07 15:58:55 +0530

    Added comments in day2.ts file for it to run

--- Dec 6th - Day 2/day2.ts
+++ Dec 6th - Day 2/day2.ts
@@ -1,7 +1,7 @@
 // Define a class in TypeScript
 class Point{
-    xAxis!: number;
-    yAxis!: number;
+    xAxis?: number;  // The question mark says that xAxis field is optional
+    yAxis?: number;
 }
 
 // Declaration of an interface
@@ -9,3 +9,14 @@ interface Mobile{
     modelNumber: string;
     modelName: string;
 }
+
+// Implementation of the Mobile interface
+class p implements Mobile{
+    modelNumber!: string; // The exclamation mark after the variable name indicates that the value is always truthy and never falsy
+    modelName!: string;
+    
+}
+
+// These are naive TypeScript declarations, 
+//they are not intended to yeild any form of output whatsoever, 
+//so no command to run these TypeScript files.
