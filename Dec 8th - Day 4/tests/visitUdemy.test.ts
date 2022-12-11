546b62f60b07e221376a5fe92b49b1e3959b7496
sulaimanahmedz-katabat <98012991+sulaimanahmedz-katabat@users.noreply.github.com>
2022-12-11 20:34:17 +0530

    Added creds for login

--- Dec 8th - Day 4/tests/visitUdemy.test.ts
+++ Dec 8th - Day 4/tests/visitUdemy.test.ts
@@ -6,22 +6,17 @@ describe("Visit Udemy and Login", () => {
         const browser = await chromium.launch({ headless: false, args: ["--start-maximized"] });
         const context = await browser.newContext({ viewport: null });
         const page = await context.newPage();
-        await page.goto("file:///C:/Users/Sulaiman.Zameer/Playwright/Playwright-Training/Dec%208th%20-%20Day%204/tests/index.html");
+        const pageURL = __dirname + "\\index.html";
+        await page.goto(pageURL);
 
-        // One way to set files is to use the event listener.
-        // await page.on("filechooser", async (filechooser: FileChooser) => {
-        //     if (await filechooser.isMultiple()) {
-
-        //     }
-        // });
 
         // Another way is to use the setInputFiles method on the page 
-        let filePath: string = "";
+        //let filePath: string = "";
         // await page.setInputFiles("input[name='fileChooser']", filePath)
 
-        await page.fill("input[name='email']", "sulaiman24998@gmail.com");
+        await page.fill("input[name='email']", "sulaiman@gmail.com");
 
-        await page.fill("input[name='password']", "Shakeel@786");
+        await page.fill("input[name='password']", "123456789");
 
         await page.click('button:text("Login")');
         // await sleepFunction();
