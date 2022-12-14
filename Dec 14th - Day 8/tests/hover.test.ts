74333d251356769dccff2dc0ab70dac4d5df7636
sulaimanahmedz-katabat <98012991+sulaimanahmedz-katabat@users.noreply.github.com>
2022-12-14 16:06:31 +0530

    added script for custom command run cleaned up log

--- Dec 14th - Day 8/tests/hover.test.ts
+++ Dec 14th - Day 8/tests/hover.test.ts
@@ -4,6 +4,5 @@ test("Test to check the Hover action", async ({ page }) => {
     await page.getByRole('link', { name: 'Hover' }).click();
     await page.locator("#mouse_over").hover();
     const changedText = await page.locator('#mouse_over').textContent();
-    console.log("HTML content is :" + changedText);
     expect(changedText).toBe('You did it!');
 });
