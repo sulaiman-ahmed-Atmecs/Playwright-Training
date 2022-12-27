347b7e7133d81d83c0e292fbd2431f7dad5083ce
sulaimanahmedz-katabat <98012991+sulaimanahmedz-katabat@users.noreply.github.com>
2022-12-27 16:27:43 +0530

    Added configs for report

--- Dec 21st - Day 13/playwright.config.ts
+++ Dec 21st - Day 13/playwright.config.ts
@@ -28,7 +28,7 @@ const config: PlaywrightTestConfig = {
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 8 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -39,8 +39,16 @@ const config: PlaywrightTestConfig = {
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-    baseURL:"https://jsonplaceholder.typicode.com/"
+  
+    baseURL:"https://jsonplaceholder.typicode.com/",
+    trace: 'on',
+    video: 'on',
+    screenshot: 'on',
+    ignoreHTTPSErrors: true,
+    viewport: {
+      width: 1366,
+      height: 728
+    }
   },
 
   /* Configure projects for major browsers */
